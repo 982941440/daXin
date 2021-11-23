80297bee65a5ff87eb6c27158a6db31c756355a3
jcduan <982941440@qq.com>
2021-11-23 16:15:59 +0800

    添加web模块

---
+++ testWeb.py
@@ -0,0 +1,14 @@
+from flask import Flask,url_for,request,render_template
+
+app = Flask(__name__)
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        if request.form['user'] == 'admin':
+            return 'Admin login successfully!'
+        else:
+            return 'No such user!'
+    title = request.args.get('title', 'Default')
+    return render_template('login.html', title=title)
+if __name__ == "__main__":
+    app.run(debug=True)
